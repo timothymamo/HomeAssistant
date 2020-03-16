791bf7ce135d51b6436c0356959c01f5eb35c4f0
Timothy Mamo <timothy@skyworkz.nl>
2020-03-16 15:00:12 +0100

    Making api call into one command

--- shell_commands/create_fwd_email.sh
+++ shell_commands/create_fwd_email.sh
@@ -25,6 +25,6 @@ else
 fi
 
 /usr/bin/curl -X POST -H "Authorization: Bearer ${HA_TOKEN}" \
-  -H "Content-Type: application/json" -d '{"state": '${RESP_CODE}'}' http://localhost:8123/api/states/sensor.curl_resp
-/usr/bin/curl -X POST -H "Authorization: Bearer ${HA_TOKEN}" \
-  -H "Content-Type: application/json" -d '{"state": "'"${MSG}"'"}' http://localhost:8123/api/states/sensor.curl_msg
+  -H "Content-Type: application/json" -d '{"state": '${RESP_CODE}',\
+  "attributes": {"message":"'"${MSG}"'"}}' \
+  http://localhost:8123/api/states/sensor.curl_resp
