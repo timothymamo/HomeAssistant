012113b570c0be918f7ad9256ae27c43531ad376
Timothy Mamo <timothy@skyworkz.nl>
2020-03-21 10:17:13 +0100

    Fixing the vpn check script. USE inverted commas

--- shell_commands/check_vpn.sh
+++ shell_commands/check_vpn.sh
@@ -6,19 +6,19 @@ STRING_CHECK="Your internet traffic is secure"
 VPN_URL="https://nordvpn.com/what-is-my-ip/"
 SENSOR_API="http://localhost:8123/api/states/sensor.vpn_error"
 
-VPN_RESULT=$(/usr/bin/curl ${VPN_URL} 2>/dev/null > vpn.html && grep -E -o ${STRING_CHECK} vpn.html && rm vpn.html)
+VPN_RESULT=$(/usr/bin/curl "${VPN_URL}" 2>/dev/null > vpn.html && grep -E -o "${STRING_CHECK}" vpn.html && rm vpn.html)
 
-if [[ ${VPN_RESULT} = 'Your internet traffic is secure' ]]
+if [[ "${VPN_RESULT}" = 'Your internet traffic is secure' ]]
 then
-  SENSOR_STATE=$(/usr/bin/curl -X GET -H "Authorization: Bearer ${HA_TOKEN}" ${SENSOR_API} 2>/dev/null | jq '.state')
-  if [[ ${SENSOR_STATE} != "off" ]]
+  SENSOR_STATE=$(/usr/bin/curl -X GET -H "Authorization: Bearer ${HA_TOKEN}" "${SENSOR_API}" 2>/dev/null | jq '.state')
+  if [[ "${SENSOR_STATE}" != "off" ]]
   then 
       /usr/bin/curl -X POST -H "Authorization: Bearer ${HA_TOKEN}" \
         -H "Content-Type: application/json" \
-        -d '{"state": "off","attributes": {"friendly_name":"VPN Error Check","message":"'"${VPN_RESULT}"'"}}' ${SENSOR_API}
+        -d '{"state": "off","attributes": {"friendly_name":"VPN Error Check","message":"'"${VPN_RESULT}"'"}}' "${SENSOR_API}"
   fi
 else
   /usr/bin/curl -X POST -H "Authorization: Bearer ${HA_TOKEN}" \
     -H "Content-Type: application/json" \
-    -d '{"state": "on","attributes": {"friendly_name":"VPN Error Check","message":"'"${VPN_RESULT}"'"}}' ${SENSOR_API}
+    -d '{"state": "on","attributes": {"friendly_name":"VPN Error Check","message":"'"${VPN_RESULT}"'"}}' "${SENSOR_API}"
 fi
